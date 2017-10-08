22ebb8070124675ce84f5793eee00803d740d655
lisi <153865768@qq.com>
2017-10-08 20:57:38 +0800

    zhang3 jianfa

--- src/com/atguigu/jian/Jianfa.java
+++ src/com/atguigu/jian/Jianfa.java
@@ -4,6 +4,10 @@ public class Jianfa {
 
 	public static void main(String[] args) {
 		System.out.println("剑法");
+		System.out.println("剑法1");
+		System.out.println("剑法2");
+		System.out.println("剑法3");
+		System.out.println("剑法4");
 	}
 
 }
